bb412b5b7ab7ba6b61f49206f0a6448cac542717
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-05-29 16:05:30 +0200

    add fetchpriority to bg-img

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,7 +2,7 @@ import QuickLinksSidebar from "./QuickLinksSidebar"
 
 const HeroSection = () => {
   return (
-    <div className="bg-[url('/images/main-picture.jpg')] bg-no-repeat bg-cover h-[700px]  ">
+    <div className="bg-[url('/images/main-picture.jpg')] bg-no-repeat bg-cover h-[700px] " fetchpriority="high">
       <div className='flex flex-col max-w-7xl mx-auto h-full'>
         <QuickLinksSidebar />
       </div>
