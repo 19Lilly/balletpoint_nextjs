1247af6f93465967306b4dbbea66ab0b57a77a7c
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-05-29 14:08:16 +0200

    QuickLinksSidebar change position on smaller screens

--- components/QuickLinksSidebar.js
+++ components/QuickLinksSidebar.js
@@ -21,7 +21,7 @@ const QuickLinksSidebar = () => {
   ];
 
   return (
-    <div className='flex flex-col justify-between w-fit gap-6 self-end  mt-auto p-2'>
+    <div className='flex flex-col justify-between w-fit gap-6 self-end  md:mt-auto mt-[55%] p-2 '>
       {quicklinks.map(({ title, href, id }) => {
         return <QuickLink title={title} href={href} key={id} />;
       })}
