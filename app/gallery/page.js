0a643f1d285591695d589643a9c5f6fe3113e528
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-05-30 09:09:42 +0200

    adding carousel for image to gallery

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,10 +1,19 @@
+'use client'
+
+import React from 'react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
 
 const Gallery = () => {
-    return (
-        <div className="mx-auto  h-full min-h-[700px] max-w-7xl">
-            galeria 
-        </div>
-    )
-}
+   const images = [9, 8, 7, 6, 5].map(number => ({
+     src: `https://placedog.net/${number}00/${number}00?id=${number}`,
+   }));
 
-export default Gallery
+   return (
+     <div className='mx-auto max-w-7xl w-full h-full min-h-[700px] flex justify-center carousel-container text-black '>
+       <Carousel images={images} style={{ height: 500, width: 800 }} />
+     </div>
+   );
+};
+
+export default Gallery;
