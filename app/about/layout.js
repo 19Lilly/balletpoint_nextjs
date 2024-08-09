51e0dd9dcab89afce7b23f3b0ec1d30f32a496ba
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-09 16:07:56 +0200

    add flex-rap to navigations

--- app/about/layout.js
+++ app/about/layout.js
@@ -5,8 +5,8 @@ export default function AboutLayout({ children }) {
 
   return (
     <div className='max-w-7xl mx-auto w-full p-4 space-y-4'>
-      <nav className='flex gap-6 text-xl items-center p-4 border-b-2'>
-        <Link href='/about'>Balletpoint</Link>
+      <nav className='flex gap-6 text-xl items-center p-4 border-b-2 flex-wrap'>
+        <Link href='/about'>Ballet point</Link>
         <Link href='/about/teacher'>Pedagóg</Link>
       </nav>
       {children}
--- app/trainings/layout.js
+++ app/trainings/layout.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const TrainingLayout = ({ children }) => {
   return (
     <div className='max-w-7xl mx-auto w-full p-4 space-y-4'>
-      <nav className='flex gap-6 text-xl items-center p-4 border-b-2'>
+      <nav className='flex gap-6 text-xl items-center p-4 border-b-2 flex-wrap'>
         <Link href='/trainings'>Zápis 2024/2025</Link>
         <Link href='/trainings/info'>Info</Link>
         <Link href='/trainings/timeSchedule'>Rozvrh hodín</Link>
