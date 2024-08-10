210eb9ab44793b17efd4b79f9655845329274678
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-10 18:13:48 +0200

    remove padding from carousel-container

--- app/gallery/[name]/page.js
+++ app/gallery/[name]/page.js
@@ -56,7 +56,7 @@ const singleGallery = ({ params }) => {
           Späť na zoznam
         </Link>
       </h1>
-      <div className='w-[min(70rem,98%)] h-[1000px] px-2'>
+      <div className='w-[min(70rem,98%)] h-[1000px]'>
         <Carousel images={imgs} isMaximized={true} />
       </div>
     </div>
