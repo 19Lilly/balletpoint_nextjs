4e7d8b215fda3a0c487d038e5e28c6f5c2e9704c
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-10 18:00:24 +0200

    changed width of carousel container

--- app/gallery/[name]/page.js
+++ app/gallery/[name]/page.js
@@ -56,7 +56,7 @@ const singleGallery = ({ params }) => {
           Späť na zoznam
         </Link>
       </h1>
-      <div className='w-70 h-[1000px] px-6'>
+      <div className='w-[min(70rem,98%)] h-[1000px] px-6'>
         <Carousel images={imgs} />
       </div>
     </div>
