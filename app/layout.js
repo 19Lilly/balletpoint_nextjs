3fd59d3a0e7f9c8e8279ebbbf6c833e9f8bd25de
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-11 12:14:49 +0200

    add cookie banner

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <script
+          id='cookieyes'
+          type='text/javascript'
+          src='https://cdn-cookieyes.com/client_data/a0de9d0b7887422f10cc9743/script.js'
+        />
+      </head>
       <body className='min-h-[100dvh] grid grid-rows-[auto_1fr_auto] text-xl'>
         <Navbar />
         {children}
