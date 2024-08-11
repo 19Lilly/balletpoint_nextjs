2531673048d77c09be83c706731fc2a5383a2d4b
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-11 16:03:46 +0200

    add google tag manager

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import './globals.css';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export const metadata = {
   title: 'Balletpoint landing page',
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className='min-h-[100dvh] grid grid-rows-[auto_1fr_auto] text-xl'>
+        <GoogleTagManager gtmId={process.env.GTM_ID} />
         <Navbar />
         {children}
         <Footer />
