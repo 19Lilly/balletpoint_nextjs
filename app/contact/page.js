fcb4b678f0bb77383f705b536ff09b74827cc5da
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-11 23:10:54 +0200

    add flex-wrap to div in contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <div className=' main-container'>
       <h1>Kontakt</h1>
-      <div className='grid md:grid-cols-2 md: gap-6 p-4 w-full'>
+      <div className='grid md:grid-cols-2 md:gap-6 p-4 w-full'>
         <iframe
           src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.620582012551!2d17.04990547702154!3d48.19466144718453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476c8d5fd4469fbf%3A0xa57bfaef1346bc5b!2zVGFuZcSNbsOhIMWha29sYSBEYW5jZXJz!5e0!3m2!1sen!2ssk!4v1717015128744!5m2!1sen!2ssk'
           style={{ border: 0 }}
@@ -13,7 +13,7 @@ const Contact = () => {
         ></iframe>
         <div>
           <div className='flex flex-col text-xl justify-center'>
-            <div className='flex gap-4 p-4 border-b-2'>
+            <div className='flex gap-4 p-4 border-b-2 flex-wrap'>
               <span className='font-bold'>Prevádzkovateľ:</span>
               <p>Ballet point o.z.</p>
             </div>
