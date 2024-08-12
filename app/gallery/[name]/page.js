e097206f67292db5ffd5691dd06e083ddafc5cd9
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-12 12:16:45 +0200

    save the images

--- app/gallery/[name]/page.js
+++ app/gallery/[name]/page.js
@@ -34,9 +34,7 @@ const SingleGallery = ({ params }) => {
       title: 'SND - Narodil sa chrobáčik a Rukopisy majstrov galéria',
       url: 'tripToSND',
       src: '/public/images/tripToSND',
-      numbers: [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-      ],
+      numbers: [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
     },
   ];
 
