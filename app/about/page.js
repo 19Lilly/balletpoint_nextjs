a256c99976dce8b81234d0ce0c8da005e5275437
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-13 15:20:43 +0200

    about: p - change width of paragraph from 70ch to 80ch

--- app/about/page.js
+++ app/about/page.js
@@ -5,22 +5,22 @@ const About = () => {
     <div className='main-container select-none'>
       <h1 className=''>Ballet Point</h1>
 
-      <p className='text-pretty md:w-[70ch] '>
+      <p className='text-pretty md:w-[80ch] '>
         Ballet point som založila v apríli 2023 pretože verím, že tak ako som ja
         začala moju tanečnú kariéru v Lamači, aj ďalšie talentované deti musia
         dostať túto príležitosť.
       </p>
-      <p className='text-pretty md:w-[70ch]'>
+      <p className='text-pretty md:w-[80ch]'>
         V Ballet point máme dôležité predsavzatie.. ukázať klasický tanec-balet
         ľuďom tak, ako ho ešte nepoznajú a priblížiť techniky moderného tanca
         ktorý má obrovský výrazový potenciál.
       </p>
-      <p className='text-pretty md:w-[70ch]'>
+      <p className='text-pretty md:w-[80ch]'>
         Tanec je súčasťou života človeka od nepamäti. Bol s nami dokonca skôr
         ako hovorené slovo. Často vieme pohybom-tancom vyjadriť emócie, na ktoré
         slová nestačia.
       </p>
-      <p className='text-pretty md:w-[70ch]'>
+      <p className='text-pretty md:w-[80ch]'>
         V Ballet point robíme presne toto-rozprávame pohybom tvoríme umenie.{' '}
       </p>
       <div className='md:w-[80ch] flex flex-col gap-2 '>
