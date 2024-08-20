420268fdb9f4d1b65c12cfe47c9a78ef47fee436
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-08-20 21:17:05 +0200

    delete unnecessary files, changed icons in gallery list for thumbnail images

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,24 +1,27 @@
 'use client';
-import React from 'react';
+import React, { useId } from 'react';
 import Link from 'next/link';
-import { RiFolderImageLine } from 'react-icons/ri';
-
+import Image from 'next/image';
 
 const Gallery = () => {
   const galleryData = [
     {
+      id: useId(),
       name: 'Naše hodiny',
       url: 'ourClasses',
     },
     {
+      id: useId(),
       name: 'Záverečný koncert - 30.6.2024',
       url: 'finalConcert_30_6_2024',
     },
     {
+      id: useId(),
       name: 'Ballet point - Letný tábor 2024',
       url: 'balletPoint_summerCamp2024',
     },
     {
+      id: useId(),
       name: 'SND - Narodil sa chrobáčik a Rukopisy majstrov galéria',
       url: 'tripToSND',
     },
@@ -28,17 +31,19 @@ const Gallery = () => {
     <div className='main-container'>
       <h1>Galéria</h1>
       <ul className='grid md:grid-cols-2'>
-        {galleryData.map((gallery, index) => {
+        {galleryData.map(({ id, name, url }) => {
           return (
-            <li key={index}>
+            <li key={id}>
               <Link
-                href={`/gallery/${gallery.url}`}
-                className='grid grid-cols-3 border-b-2 p-4 w-[min(30rem,98%)] hover:text-fuchsia-600'
+                href={`/gallery/${url}`}
+                className='grid grid-cols-3 gap-4 border-b-2 p-4 w-[min(30rem,98%)] hover:text-fuchsia-600'
               >
-                <RiFolderImageLine className='size-20 col-span-1' />
-                <h2 className='text-2xl col-span-2 self-center'>
-                  {gallery.name}
-                </h2>
+                <Image
+                  src={`/images/${url}/thumbnail.jpg`}
+                  width={250}
+                  height={250}
+                />
+                <h2 className='text-2xl col-span-2 self-center'>{name}</h2>
               </Link>
             </li>
           );
