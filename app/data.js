3aca5bce127dca514887bc998b9238532c7b44f0
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-09-15 09:55:54 +0200

    reorganizerd the photo from event

--- app/data.js
+++ app/data.js
@@ -180,11 +180,11 @@ const galleryData = [
     numbers: [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
   },
   {
-    id: uuid(), 
-    title: 'Rozálske hody', 
+    id: uuid(),
+    title: 'Rozálske hody',
     url: 'RozalskeHody',
-    numbers:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
-  }
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+  },
 ];
 
 const eventsData = [
