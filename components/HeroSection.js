3a731dfd0a3e3a9541445946ddcf8646ed451c4a
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2024-12-22 10:55:09 +0100

    add snowfall animation

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,4 +1,6 @@
+'use client';
 import QuickLinksSidebar from './QuickLinksSidebar';
+import Snowfall from 'react-snowfall';
 
 const HeroSection = () => {
   return (
@@ -6,6 +8,7 @@ const HeroSection = () => {
       className="bg-[url('/images/main-picture1.jpg')] bg-no-repeat  bg-contain bg-center xl:bg-left relative bg-slate-200"
       fetchPriority='high'
     >
+      <Snowfall color='white' snowflakeCount={100} />
       <div className='absolute bottom-0  w-full  bg-slate-200 xl:top-1/5 xl:right-1/4 xl:w-[25rem] xl:h-full text-center text-3xl xl:text-6xl p-4 pt-0 xl:pt-14 '>
         <p>
           Pokojné Vianočné sviatky Vám želá <br />
