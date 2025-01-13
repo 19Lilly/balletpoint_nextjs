0db4454e65b95fab4e8384840eaa0676326b81fb
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2025-01-13 22:52:47 +0100

    pre holiday season set up

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -5,17 +5,11 @@ import Snowfall from 'react-snowfall';
 const HeroSection = () => {
   return (
     <div
-      className="bg-[url('/images/main-picture1.jpg')] bg-no-repeat  bg-contain bg-center xl:bg-left relative bg-slate-200"
+      className="bg-[url('/images/main-picture.jpg')] bg-no-repeat  bg-cover  
+      h-100vh w-full relative"
       fetchPriority='high'
     >
-      <Snowfall color='white' snowflakeCount={100} style={{ zIndex: 10 }} />
-      <div className='absolute bottom-0  w-full  bg-slate-200 xl:top-1/5 xl:right-1/4 xl:w-[25rem] xl:h-full text-center text-3xl xl:text-6xl p-4 pt-0 xl:pt-14 '>
-        <p>
-          Pokojné Vianočné sviatky Vám želá <br />
-          Ballet point
-        </p>
-      </div>
-      <div className='main-container flex flex-col h-full  relative invisible xl:visible'>
+      <div className='main-container flex flex-col h-full  relative '>
         <QuickLinksSidebar />
       </div>
     </div>
