9eeb0b1746ef19fd93135d6cdf0a1071d3081c09
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2025-04-29 22:15:28 +0200

    popup with a new message

--- components/PopUp.js
+++ components/PopUp.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import { IoClose } from 'react-icons/io5';
+import Logo from './Logo';
+import Link from 'next/link';
+import { FaArrowRight } from 'react-icons/fa';
 
 const PopUp = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -16,15 +19,20 @@ const PopUp = () => {
               <IoClose />
             </button>
 
-            <img
-              src='/images/happyEaster.jpg'
-              alt='ballet point happy easter'
-              className='rounded-xl md:w-[min(20rem,90%)]'
-            />
+            <Logo className='w-1/2 md:w-1/3' />
 
-            <div className='my-4'>
-              <p>Ballet point praje krásne Veľkonočné sviatky.</p>
-              <p>Tešíme sa na Vás zas od 23.4.2025</p>
+            <div className='my-4 space-y-8'>
+              <p className='text-pretty'>
+                Dňa 2.5.2025 sa hodiny Ballet pointu uskutočnia podľa aktuálneho
+                <Link
+                  href='/trainings/timeSchedule'
+                  className='hover:text-fuchsia-600 underline inline-flex items-center gap-2 ml-2'
+                >
+                  rozvrhu <FaArrowRight className='text-base' />
+                </Link>
+              </p>
+
+              <p>Dňa 9.5.2025 hodiny baletu nebudú.</p>
             </div>
           </div>
         </div>
