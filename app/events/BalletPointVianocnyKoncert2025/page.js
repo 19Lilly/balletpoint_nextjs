7eb41ea8d0001ed2303aba2913416212fcc49891
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2025-11-04 23:33:31 +0100

    changed of title

--- app/events/BalletPointVianocnyKoncert2025/page.js
+++ app/events/BalletPointVianocnyKoncert2025/page.js
@@ -10,17 +10,18 @@ export const metadata = {
 const VianocnyKoncert2025 = () => {
   return (
     <div className='main-container'>
-      <h1>Vianočný Koncert Ballet pointu - 19.12.2025</h1>
-   
+      <h1>Vianočný Koncert</h1>
+
       <div className='main-container flex items-center justify-center mx-auto'>
-        <p className='text-2xl'><span className='font-bold'>Vstupné:</span> 4€</p>
+        <p className='text-2xl'>
+          <span className='font-bold'>Vstupné:</span> 4€
+        </p>
         <Image
           src={poster}
           width={700}
           height={500}
           alt='Plagát s informáciami o vianočnom koncerte žiakov Ballet pointu Lamač 19.12.2025'
         />
-        
       </div>
     </div>
   );
