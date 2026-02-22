33bea1af709ff53a20adc084b2a05fe3c1229335
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2026-02-22 15:52:49 +0100

    added pay by square

--- app/workshop/page.js
+++ app/workshop/page.js
@@ -7,8 +7,22 @@ import React from 'react'
 
 const Workshop = () => {
   return (
-    <div className='main-container select-none'>
+    <div className='main-container'>
       <h1 className=''>Workshop</h1>
+      <p>
+        Prihlasovanie:
+        <a
+          href='mailto:info@balletpoint.sk'
+          className='underline underline-offset-2 hover:text-[#cca300] px-2'
+        >
+          info@balletpoint.sk
+        </a>
+      </p>
+      <img
+        src='/images/payBySquareWorkshop.jpeg'
+        alt='pay by square image'
+        className='aspect-square size-28 mt-4 mx-auto md:mx-0'
+      />
       <div className='flex flex-col md:flex-row'>
         <Image
           src={poster}
@@ -23,7 +37,6 @@ const Workshop = () => {
           alt='Plagát s informáciami o workshope'
         />
       </div>
-
       <video controls autoPlay muted loop height={700}>
         <source src='/videos/workshop.mp4' type='video/mp4' />
         Your browser does not support the video tag.
